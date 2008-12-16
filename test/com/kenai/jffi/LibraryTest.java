583a3e128648745c892aceb50a88e2abf92fe33e
Wayne Meissner <wmeissner@gmail.com>
2008-12-16 14:59:19 +1000

    Add unit test for dlopen

---
+++ test/com/kenai/jffi/LibraryTest.java
@@ -0,0 +1,45 @@
+package com.kenai.jffi;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author wayne
+ */
+public class LibraryTest {
+
+    public LibraryTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test public void dlopen() {
+        long handle = Foreign.getForeign().dlopen("libc.so.6", Library.LAZY | Library.GLOBAL);
+        assertNotSame("Could not open libc.so", 0L, handle);
+    }
+}
