c044dfc18e4e770fecb90b0d2ba1f40698a799fe
Wayne Meissner <wmeissner@gmail.com>
2008-12-18 14:47:06 +1000

    Add native memory I/O operations

---
+++ jni/jffi/MemoryIO.c
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2007 Wayne Meissner
+ *
+ * This file is part of jffi.
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * version 3 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <jni.h>
+#include "jffi.h"
+#include "com_kenai_jffi_MemoryIO.h"
+#include "com_kenai_jffi_MemoryIO_Native.h"
+
+#define GET(JTYPE, NTYPE) JNIEXPORT NTYPE JNICALL \
+Java_com_kenai_jffi_MemoryIO_00024Native_get##JTYPE(JNIEnv* env, jobject self, jlong address) \
+{ NTYPE tmp; memcpy(&tmp, j2p(address), sizeof(tmp)); return tmp; }
+
+#define PUT(JTYPE, NTYPE) \
+JNIEXPORT void JNICALL \
+Java_com_kenai_jffi_MemoryIO_00024Native_put##JTYPE(JNIEnv *env, jobject self, jlong address, NTYPE value) \
+{ memcpy(j2p(address), &value, sizeof(value)); }
+
+#define COPY(JTYPE, NTYPE) \
+JNIEXPORT void JNICALL \
+Java_com_kenai_jffi_MemoryIO_put##JTYPE##Array(JNIEnv* env, jobject unsafe, jlong address, jobject obj, jint offset, jint length) \
+{ \
+    (*env)->Get##JTYPE##ArrayRegion(env, obj, offset, length, (NTYPE *) j2p(address)); \
+} \
+JNIEXPORT void JNICALL \
+Java_com_kenai_jffi_MemoryIO_get##JTYPE##Array(JNIEnv* env, jobject unsafe, jlong address, jobject obj, jint offset, jint length) \
+{ \
+    (*env)->Set##JTYPE##ArrayRegion(env, obj, offset, length, (NTYPE *) j2p(address)); \
+}
+#define UNSAFE(J, N) GET(J, N) PUT(J, N) COPY(J, N)
+
+UNSAFE(Byte, jbyte);
+UNSAFE(Char, jchar);
+UNSAFE(Boolean, jboolean);
+UNSAFE(Short, jshort);
+UNSAFE(Int, jint);
+UNSAFE(Long, jlong);
+UNSAFE(Float, jfloat);
+UNSAFE(Double, jdouble);
+
+/*
+ * Class:     com_googlecode_jffi_JNIUnsafe
+ * Method:    getAddress
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL
+Java_com_kenai_jffi_MemoryIO_00024Native_getAddress(JNIEnv* ev, jobject self, jlong address)
+{
+    void* tmp;
+    memcpy(&tmp, j2p(address), sizeof(tmp));
+    return p2j(tmp);
+}
+
+/*
+ * Class:     com_googlecode_jffi_JNIUnsafe
+ * Method:    putAddress
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL
+Java_com_kenai_jffi_MemoryIO_00024Native_putAddress(JNIEnv* env, jobject self, jlong address, jlong value)
+{
+    void* tmp = j2p(value);
+    memcpy(j2p(address), &tmp, sizeof(tmp));
+}
+
+/*
+ * Class:     com_googlecode_jffi_Unsafe_JNIUnsafe
+ * Method:    setMemory
+ * Signature: (JJB)V
+ */
+JNIEXPORT void JNICALL
+Java_com_kenai_jffi_MemoryIO_00024Native_setMemory(JNIEnv* env, jobject self, jlong address, jlong size, jbyte value)
+{
+    memset(j2p(address), value, size);
+}
+
+/*
+ * Class:     com_googlecode_jffi_lowlevel_Unsafe_JNIUnsafe
+ * Method:    copyMemory
+ * Signature: (JJJ)V
+ */
+JNIEXPORT void JNICALL
+Java_com_kenai_jffi_MemoryIO_00024Native_copyMemory(JNIEnv* env, jobject self, jlong src, jlong dst, jlong size)
+{
+    memcpy(j2p(dst), j2p(src), size);
+}
+
+/*
+ * Class:     com_googlecode_jffi_lowlevel_Unsafe
+ * Method:    memchr
+ * Signature: (JIJ)I
+ */
+JNIEXPORT jlong JNICALL
+Java_com_kenai_jffi_Unsafe_memchr(JNIEnv* env, jobject self, jlong address, jint c, jlong maxlen)
+{
+    void* ptr = memchr(j2p(address), c, maxlen);
+    if (ptr == NULL) {
+        return -1;
+    }
+    return (int) (p2j(ptr) - address);
+}
---
+++ src/com/kenai/jffi/MemoryIO.java
@@ -0,0 +1,68 @@
+
+package com.kenai.jffi;
+
+public abstract class MemoryIO {
+    private static final class SingletonHolder {
+        static {
+            Init.init();
+        }
+        private static final MemoryIO INSTANCE = getImpl();
+    }
+    public static MemoryIO getMemoryIO() {
+        return SingletonHolder.INSTANCE;
+    }
+    private MemoryIO() {}
+    
+    public abstract byte getByte(long address);
+    public abstract short getShort(long address);
+    public abstract int getInt(long address);
+    public abstract long getLong(long address);
+    public abstract float getFloat(long address);
+    public abstract double getDouble(long address);
+    public abstract long getAddress(long address);
+    public abstract void putByte(long address, byte value);
+    public abstract void putShort(long address, short value);
+    public abstract void putInt(long address, int value);
+    public abstract void putLong(long address, long value);
+    public abstract void putFloat(long address, float value);
+    public abstract void putDouble(long address, double value);
+    public abstract void putAddress(long address, long value);
+    public abstract void copyMemory(long src, long dst, long size);
+    public abstract void setMemory(long src, long size, byte value);
+    public native void putByteArray(long address, byte[] data, int offset, int length);
+    public native void getByteArray(long address, byte[] data, int offset, int length);
+    public native void putCharArray(long address, char[] data, int offset, int length);
+    public native void getCharArray(long address, char[] data, int offset, int length);
+    public native void putShortArray(long address, short[] data, int offset, int length);
+    public native void getShortArray(long address, short[] data, int offset, int length);
+    public native void putIntArray(long address, int[] data, int offset, int length);
+    public native void getIntArray(long address, int[] data, int offset, int length);
+    public native void getLongArray(long address, long[] data, int offset, int length);
+    public native void putLongArray(long address, long[] data, int offset, int length);
+    public native void getFloatArray(long address, float[] data, int offset, int length);
+    public native void putFloatArray(long address, float[] data, int offset, int length);
+    public native void getDoubleArray(long address, double[] data, int offset, int length);
+    public native void putDoubleArray(long address, double[] data, int offset, int length);
+    
+    private static final class Native extends MemoryIO {
+        public native byte getByte(long address);
+        public native short getShort(long address);
+        public native int getInt(long address);
+        public native long getLong(long address);
+        public native float getFloat(long address);
+        public native double getDouble(long address);
+        public native long getAddress(long address);
+        public native void putByte(long address, byte value);
+        public native void putShort(long address, short value);
+        public native void putInt(long address, int value);
+        public native void putLong(long address, long value);
+        public native void putFloat(long address, float value);
+        public native void putDouble(long address, double value);
+        public native void putAddress(long address, long value);
+        public native void setMemory(long address, long size, byte value);
+        public native void copyMemory(long src, long dst, long size);
+    }
+    private static final MemoryIO getImpl() {
+        return new Native();
+    }
+}
