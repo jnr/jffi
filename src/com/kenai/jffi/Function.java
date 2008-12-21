a1bd03dfc80c5ddfae360a2fadf3d92cd1227e33
Wayne Meissner <wmeissner@gmail.com>
2008-12-21 14:17:24 +1000

    Free the native function handle when being garbage collected

--- src/com/kenai/jffi/Function.java
+++ src/com/kenai/jffi/Function.java
@@ -1,31 +1,38 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.kenai.jffi;
 
-/**
- *
- * @author wayne
- */
 public final class Function {
     private final int address32;
     private final Address handle;
+
     public Function(Address address, NativeType returnType, NativeType[] paramTypes) {
         int[] nativeParamTypes = new int[paramTypes.length];
         for (int i = 0; i < paramTypes.length; ++i) {
             nativeParamTypes[i] = paramTypes[i].value();
         }
-        long h = Foreign.getForeign().newFunction(address.nativeAddress(), returnType.value(), nativeParamTypes, 0);
+        final long h = Foreign.getForeign().newFunction(address.nativeAddress(),
+                returnType.value(), nativeParamTypes, 0);
+        if (h == 0) {
+            throw new RuntimeException("Failed to create native function");
+        }
         handle = new Address(h);
-        address32 = (int) handle.nativeAddress();
+        address32 = (int) h;
     }
-
+    
     final long getAddress64() {
         return handle.nativeAddress();
     }
     final int getAddress32() {
         return address32;
+    }
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            if (handle != null && !handle.isNull()) {
+                Foreign.getForeign().freeFunction(handle.nativeAddress());
+            }
+        } finally {
+            super.finalize();
+        }
     }
 }
