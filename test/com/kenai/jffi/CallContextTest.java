a50a44d87d9c5e699021b26025d34be6fa5749b5
Wayne Meissner <wmeissner@gmail.com>
2008-12-21 19:43:01 +1000

    Use Invoker.getInstance() instead of Foreign.getForeign()

--- test/com/kenai/jffi/CallContextTest.java
+++ test/com/kenai/jffi/CallContextTest.java
@@ -43,30 +43,30 @@ public class CallContextTest {
     // @Test
     // public void hello() {}
     @Test public void VrV() {
-        long handle = Foreign.getForeign().newCallContext(NativeType.VOID.value(),
-                new int[] { NativeType.VOID.value() }, 0);
+        long handle = Foreign.getForeign().newCallContext(Type.VOID.value(),
+                new int[] { Type.VOID.value() }, 0);
         assertNotSame("Foreign#newCallContext failed", 0L, handle);
-        CallContext ctx = new CallContext(NativeType.VOID, new NativeType[] { NativeType.VOID });
+        CallContext ctx = new CallContext(Type.VOID, new Type[] { Type.VOID });
     }
     @Test public void VrI() {
-        long handle = Foreign.getForeign().newCallContext(NativeType.INT.value(),
-                new int[] { NativeType.VOID.value() }, 0);
+        long handle = Foreign.getForeign().newCallContext(Type.INT.value(),
+                new int[] { Type.VOID.value() }, 0);
         assertNotSame("Foreign#newCallContext failed", 0L, handle);
     }
     @Test public void IrI() {
-        long handle = Foreign.getForeign().newCallContext(NativeType.INT.value(),
-                new int[] { NativeType.INT.value() }, 0);
+        long handle = Foreign.getForeign().newCallContext(Type.INT.value(),
+                new int[] { Type.INT.value() }, 0);
         assertNotSame("Foreign#newCallContext failed", 0L, handle);
     }
     @Test public void getpid() {
 
         long libc = Foreign.getForeign().dlopen("libc.so.6", Library.LAZY | Library.LOCAL);
         long getpid = Foreign.getForeign().dlsym(libc, "getpid");
-        Function f = new Function(new Address(getpid), NativeType.SINT32,
-                new NativeType[] { });
-        int pid = Foreign.getForeign().invokeVrI(f);
+        Function f = new Function(new Address(getpid), Type.SINT32,
+                new Type[] { });
+        int pid = Invoker.getInstance().invokeVrI(f);
         System.out.println("pid=" + pid);
         HeapInvocationBuffer paramBuffer = new HeapInvocationBuffer(1);
-        pid = Foreign.getForeign().invokeInt(f, paramBuffer);
+        pid = Invoker.getInstance().invokeInt(f, paramBuffer);
     }
 }
