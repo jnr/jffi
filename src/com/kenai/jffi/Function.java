cea607699b8264c3e7a4d557fc4e0a373dc5f964
Wayne Meissner <wmeissner@gmail.com>
2008-12-27 23:19:16 +1000

    Add a Function constructor that takes the calling convention

--- src/com/kenai/jffi/Function.java
+++ src/com/kenai/jffi/Function.java
@@ -7,13 +7,14 @@ public final class Function {
     private final int parameterCount;
     private final int rawParameterSize;
     
-    public Function(Address address, Type returnType, Type[] paramTypes) {
+    public Function(Address address, Type returnType, Type[] paramTypes, CallingConvention convention) {
         int[] nativeParamTypes = new int[paramTypes.length];
         for (int i = 0; i < paramTypes.length; ++i) {
             nativeParamTypes[i] = paramTypes[i].value();
         }
         final long h = Foreign.getInstance().newFunction(address.nativeAddress(),
-                returnType.value(), nativeParamTypes, 0);
+                returnType.value(), nativeParamTypes,
+                convention == CallingConvention.STDCALL ? 1 : 0);
         if (h == 0) {
             throw new RuntimeException("Failed to create native function");
         }
@@ -21,6 +22,9 @@ public final class Function {
         rawParameterSize = Foreign.getInstance().getFunctionRawParameterSize(h);
         address64 = h;
         address32 = (int) h;
+    }
+    public Function(Address address, Type returnType, Type[] paramTypes) {
+        this(address, returnType, paramTypes, CallingConvention.DEFAULT);
     }
     final int getParameterCount() {
         return parameterCount;
