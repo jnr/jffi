4263505fe3c413a870bb0519125f845513024d6d
Wayne Meissner <wmeissner@gmail.com>
2009-04-01 17:51:32 +1000

    Keep strong references to the return type and parameter types so they do not get garbage collected until the function does.
    Sanity check the return and parameter types, and only allow builtins through for now

--- src/com/kenai/jffi/Function.java
+++ src/com/kenai/jffi/Function.java
@@ -6,18 +6,38 @@ public final class Function {
     private final long address;
     private final int parameterCount;
     private final int rawParameterSize;
+    private final Type returnType;
+    private final Type[] paramTypes;
     
     public Function(long address, Type returnType, Type[] paramTypes, CallingConvention convention) {
+        
         long[] nativeParamTypes = new long[paramTypes.length];
         for (int i = 0; i < paramTypes.length; ++i) {
+            if (!(paramTypes[i] instanceof Type.Builtin)) {
+                throw new IllegalArgumentException("parameter type " + paramTypes[i] + " is not supported");
+            }
             nativeParamTypes[i] = paramTypes[i].handle();
         }
+
+        if (!(returnType instanceof Type.Builtin)) {
+            throw new IllegalArgumentException("return type " + returnType + " is not supported");
+        }
+
         final long h = Foreign.getInstance().newFunction(address,
                 returnType.handle(), nativeParamTypes,
                 convention == CallingConvention.STDCALL ? 1 : 0);
         if (h == 0) {
             throw new RuntimeException("Failed to create native function");
         }
+
+        //
+        // Keep references to the return and parameter types so they do not get
+        // garbage collected
+        //
+        this.returnType = returnType;
+        this.paramTypes = new Type[paramTypes.length];
+        System.arraycopy(paramTypes, 0, this.paramTypes, 0, paramTypes.length);
+
         this.parameterCount = nativeParamTypes.length;
         this.rawParameterSize = Foreign.getInstance().getFunctionRawParameterSize(h);
         this.address = h;
