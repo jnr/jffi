292a33f170960f1a66e7b97e06295b525c914c07
Wayne Meissner <wmeissner@gmail.com>
2009-04-04 00:00:35 +1000

    Add javadoc to LastError

--- src/com/kenai/jffi/LastError.java
+++ src/com/kenai/jffi/LastError.java
@@ -1,16 +1,34 @@
 
 package com.kenai.jffi;
 
+/**
+ * Provides access to the value of errno on unix, or GetLastError on windows.
+ */
 public final class LastError {
-    private final Foreign foreign = Foreign.getInstance();
+
+    /** Lazy-initialization singleton holder */
     private static final class SingletonHolder {
         static final LastError INSTANCE = new LastError();
     }
+
+    /** Creates a new <tt>LastError</tt> instance */
     private LastError() {}
+
+    /**
+     * Gets the singleton instance of the <tt>LastError</tt> object.
+     *
+     * @return An instance of <tt>LastError</tt>
+     */
     public static final LastError getInstance() {
         return SingletonHolder.INSTANCE;
     }
+
+    /**
+     * Gets the errno set by the last C function invoked by the current thread.
+     *
+     * @return The value of errno/GetLastError()
+     */
     public final int getError() {
-        return foreign.getLastError();
+        return Foreign.getInstance().getLastError();
     }
 }
