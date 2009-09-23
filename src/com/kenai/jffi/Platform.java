58a1368846c1c7f11f38ab5b3476d16650584309
Wayne Meissner <wmeissner@gmail.com>
2009-09-24 00:08:19 +1000

    Add S390-Linux support

--- src/com/kenai/jffi/Platform.java
+++ src/com/kenai/jffi/Platform.java
@@ -77,6 +77,8 @@ public abstract class Platform {
         SPARC,
         /** Sun sparc 64 bit */
         SPARCV9,
+        /** IBM zSeries S/390 64 bit */
+        S390X,
         /** Unknown CPU */
         UNKNOWN;
 
@@ -152,13 +154,11 @@ public abstract class Platform {
             return CPU.X86_64;
         } else if ("ppc".equals(archString) || "powerpc".equals(archString)) {
             return CPU.PPC;
-        } else if ("ppc64".equals(archString)) {
-            return CPU.PPC64;
-        } else if ("sparc".equals(archString)) {
-            return CPU.SPARC;
-        } else if ("sparcv9".equals(archString)) {
-            return CPU.SPARCV9;
-        } else {
+        }
+        // Try to find by lookup up in the CPU list
+        try {
+            return CPU.valueOf(archString.toUpperCase());
+        } catch (IllegalArgumentException ex) {
             throw new ExceptionInInitializerError("Unsupported CPU architecture: " + archString);
         }
     }
@@ -189,6 +189,7 @@ public abstract class Platform {
                 case X86_64:
                 case PPC64:
                 case SPARCV9:
+                case S390X:
                     dataModel = 64;
                     break;
                 default:
