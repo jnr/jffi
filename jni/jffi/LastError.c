73b5d7cdab22d4353dd32e8ac50f213652da17d0
Vladimir Sizikov <vsizikov@gmail.com>
2009-12-27 11:24:09 +0100

    JRUBY-4388: [FFI] GetLastError always returns 0 on Windows
    *NOTE*: This requires GCC 4.4+ in order to have thread-locals work
    (needed for errno handling).

--- jni/jffi/LastError.c
+++ jni/jffi/LastError.c
@@ -6,8 +6,8 @@
 #include <jni.h>
 #include "LastError.h"
 
-#if defined(_WIN32) && defined(notyet)
-static int __thread last_error = 0;
+#if defined(_WIN32)
+static __thread int last_error = 0;
 #endif
 
 /*
@@ -19,11 +19,8 @@ JNIEXPORT jint JNICALL
 Java_com_kenai_jffi_Foreign_getLastError(JNIEnv* env, jobject self)
 {
 #ifdef _WIN32
-#ifdef notyet
+    // printf("Getting ERRNO: %d on thread %d\n", last_error, (int)GetCurrentThreadId());
     return last_error;
-#else
-    return GetLastError();
-#endif
 #else
     return thread_data_get()->error;
 #endif
@@ -38,7 +35,9 @@ JNIEXPORT void JNICALL
 Java_com_kenai_jffi_Foreign_setLastError(JNIEnv* env, jobject self, jint value)
 {
 #ifdef _WIN32
+    // printf("Setting ERRNO: %d on thread %d\n", value, (int)GetCurrentThreadId());
     SetLastError(value);
+    last_error = value;
 #else
     thread_data_get()->error = errno = value;
 #endif
@@ -48,9 +47,9 @@ void
 jffi_save_errno(void)
 {
 #ifdef _WIN32
-    //last_error = GetLastError();
+    last_error = GetLastError();
+    // printf("JFFI Saving ERRNO: %d on thread %d\n", last_error, (int)GetCurrentThreadId());
 #else
     thread_data_get()->error = errno;
 #endif
 }
-
