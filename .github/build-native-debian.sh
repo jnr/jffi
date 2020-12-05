504a0a7802ce9aa3da0d1e96753f34b07a1a2f33
Tobias Gruetzmacher <tobias-git@23.gs>
2020-12-05 18:09:04 +0100

    Introduce matrix for multiple architecture builds

--- .github/build-native-debian.sh
+++ .github/build-native-debian.sh
@@ -5,7 +5,7 @@ set -ex
 cd "$(dirname "$(dirname "$0")")"
 
 apt-get update -y
-apt-get install -y --no-install-recommends openjdk-8-jdk-headless ant make gcc libc6-dev
+apt-get install -y --no-install-recommends openjdk-8-jdk-headless ant make gcc libc6-dev texinfo
 rm archive/*
 ant jar && ant archive-platform-jar
 
