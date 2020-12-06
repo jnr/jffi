ee067e8497d69c127e10104276e838c25071af4e
Tobias Gruetzmacher <tobias-git@23.gs>
2020-12-06 01:14:19 +0100

    Update native docker builds to Debian 10

--- .github/build-native-debian.sh
+++ .github/build-native-debian.sh
@@ -4,8 +4,16 @@ set -ex
 
 cd "$(dirname "$(dirname "$0")")"
 
+# Add stretch for Java 8
+cat <<END > /etc/apt/sources.list.d/stretch.list
+deb http://deb.debian.org/debian stretch main
+deb http://security.debian.org/debian-security stretch/updates main
+END
+
 apt-get update -y
-apt-get install -y --no-install-recommends openjdk-8-jdk-headless ant make gcc libc6-dev texinfo
+apt-get install -y --no-install-recommends openjdk-8-jdk-headless make gcc libc6-dev texinfo
+# Needs to be split, otherwise a newer version of OpenJDK is pulled
+apt-get install -y --no-install-recommends ant
 rm archive/*
 ant jar && ant archive-platform-jar
 
